14523a3a6b6c42a43e055d3cd091165848b97c1f
Badmos <thesaraswatyash@gmail.com>
2025-07-12 02:12:59 +0530

    url

--- legalai-ui/utils/meta.ts
+++ legalai-ui/utils/meta.ts
@@ -1,7 +1,7 @@
-// client/utils/meta.ts
+
 export async function saveMeta(docid: string, query: string) {
   try {
-    await fetch("http://localhost:8000/meta", {
+    await fetch(`https://maamla-legal-hai-api.onrender.com/meta`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ docid, query }),
